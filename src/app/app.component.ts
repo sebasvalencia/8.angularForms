501d3a4906b1eb3d7d34ac2ff2f928ea10896198
Sebastian Valencia <sebastian.valencia@yuxiglobal.com>
2018-12-05 17:00:35 -0500

    Create a form with reactiveFormModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularReactiveForms';
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder){
+
+    this.form = this.fb.group({
+      username: [''], // , Validators.required.forbiddenNameValidator()],
+      password: '',
+      age: [0, Validators.max(5)]
+    });
+
+    this.form.patchValue({
+      username: 'juan',
+      password: '1234'
+    });
+
+  }
+
+  onSubmit(event) {
+    console.log(event);
+  }
+
+
 }
